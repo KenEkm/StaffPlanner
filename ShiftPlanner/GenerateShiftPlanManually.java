f638d04b948ea5175a28795661a5d08cd7e1aa22
KenEkm <kenankeni@web.de>
2023-07-20 01:36:33 +0200

    added basic logic for afternoon shift
    checks if staff worked in morning shift, date, preferred position.

--- ShiftPlanner/GenerateShiftPlanManually.java
+++ ShiftPlanner/GenerateShiftPlanManually.java
@@ -7,17 +7,25 @@ public class GenerateShiftPlanManually {
     public ArrayList<String> workingDaysTuTh = new ArrayList<>();
 
     public ArrayList<String> prefPosAll = new ArrayList<>();
+    public ArrayList<String> prefPosRecOrg = new ArrayList<>();
+    public ArrayList<String> prefPosPzr = new ArrayList<>();
     public Worker helga;
     public Worker maxi;
     public Worker phoebe;
     public Worker rachel;
     public Worker monica;
+    public Worker lilly;
+    public Worker chandler;
+    public Worker joey;
+    public Worker ross;
+    public Worker william;
+    public Worker louis;
+    public Worker peter;
     public LocalDate date;
 
-    public void initializeMorningShift(){
+    public void initializeMorningShift(MorningShift morningShift){
         initialize();
 
-        MorningShift morningShift = new MorningShift(date = LocalDate.now(), new int[]{2, 1, 2, 1});
         String weekDay = String.valueOf(morningShift.getDate().getDayOfWeek());
         System.out.println(weekDay);
 
@@ -26,7 +34,29 @@ public class GenerateShiftPlanManually {
         addWorkerToMorningShift(morningShift, phoebe, weekDay);
         addWorkerToMorningShift(morningShift, rachel, weekDay);
         addWorkerToMorningShift(morningShift, monica, weekDay);
+        addWorkerToMorningShift(morningShift, lilly, weekDay);
         morningShift.printAllWorkersMorningShift();
+    }
+
+    public void initializeAfternoonShift(MorningShift morningShift, AfternoonShift afternoonShift){
+
+        String weekDay = String.valueOf(afternoonShift.getDate().getDayOfWeek());
+        System.out.println(weekDay);
+
+        addWorkerToAfternoonShift(morningShift, afternoonShift, helga, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, maxi, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, phoebe, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, rachel, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, monica, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, lilly, weekDay);
+
+        addWorkerToAfternoonShift(morningShift, afternoonShift, chandler, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, joey, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, ross, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, william, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, louis, weekDay);
+        addWorkerToAfternoonShift(morningShift, afternoonShift, peter, weekDay);
+        afternoonShift.printAllWorkersAfternoonShift();
     }
 
     public void addWorkerToMorningShift(MorningShift morningShift, Worker worker, String weekDay){
@@ -34,15 +64,46 @@ public class GenerateShiftPlanManually {
         if(worker.isMorningShift() && checkWorkingDay(worker,weekDay)){
             if(checkPrefPos(worker, "ass") && morningShift.getWorkersAss().size() < morningShift.getSpecReqStaffCount(0)) {
                 morningShift.addWorkersAss(worker);
+                morningShift.addAllWorkersMorningShift(worker);
             }
             else if(checkPrefPos(worker, "rec") && morningShift.getWorkersRec().size() < morningShift.getSpecReqStaffCount(1)){
                 morningShift.addWorkersRec(worker);
+                morningShift.addAllWorkersMorningShift(worker);
             }
             else if(checkPrefPos(worker, "pzr") && morningShift.getWorkersPzr().size() < morningShift.getSpecReqStaffCount(2)){
                 morningShift.addWorkersPzr(worker);
+                morningShift.addAllWorkersMorningShift(worker);
             }
             else if(checkPrefPos(worker, "org") && morningShift.getWorkersOrg().size() < morningShift.getSpecReqStaffCount(3)){
                 morningShift.addWorkersOrg(worker);
+                morningShift.addAllWorkersMorningShift(worker);
+            }
+        }
+    }
+
+    public void addWorkerToAfternoonShift(MorningShift morningShift, AfternoonShift afternoonShift, Worker worker, String weekDay){
+        if(worker.isAfternoonShift() && checkWorkingDay(worker, weekDay)){
+            ArrayList allWorkersMorn = morningShift.getAllWorkersMorningShift();
+            for(int i = 0; i < allWorkersMorn.size(); i++){
+                if(allWorkersMorn.get(i) == worker){
+                    return;
+                }
+            }
+            if(checkPrefPos(worker, "ass") && afternoonShift.getWorkersAss().size() < afternoonShift.getSpecReqStaffCount(0)){
+                afternoonShift.addWorkersAss(worker);
+                afternoonShift.addAllWorkersAfternoonShift(worker);
+            }
+            else if(checkPrefPos(worker, "rec") && afternoonShift.getWorkersRec().size() < afternoonShift.getSpecReqStaffCount(1)){
+                afternoonShift.addWorkersRec(worker);
+                afternoonShift.addAllWorkersAfternoonShift(worker);
+            }
+            else if(checkPrefPos(worker, "pzr") && afternoonShift.getWorkersPzr().size() < afternoonShift.getSpecReqStaffCount(2)){
+                afternoonShift.addWorkersPzr(worker);
+                afternoonShift.addAllWorkersAfternoonShift(worker);
+            }
+            else if(checkPrefPos(worker, "org") && afternoonShift.getWorkersOrg().size() < afternoonShift.getSpecReqStaffCount(3)){
+                afternoonShift.addWorkersOrg(worker);
+                afternoonShift.addAllWorkersAfternoonShift(worker);
             }
         }
     }
@@ -61,19 +122,21 @@ public class GenerateShiftPlanManually {
         ArrayList prefPos = worker.getPreferredPosition();
         for(int i = 0; i < prefPos.size(); i++) {
             if (prefPos.get(i).equals(pos)){
-                System.out.println(worker.getName() + " preferred position is: " + pos);
+                //System.out.println(worker.getName() + " preferred position is: " + pos);
                 return true;
             }
         }
         return false;
     }
 
-
+    //initializes environment for testing various scenarios
     private void initialize(){
         setWorkingDaysAll();
         setWorkingDaysMoWeFr();
         setWorkingDaysTuTh();
         setPrefPosAll();
+        setPrefPosPzr();
+        setPrefPosRecOrg();
         setWorker();
     }
 
@@ -103,21 +166,51 @@ public class GenerateShiftPlanManually {
         prefPosAll.add("org");
     }
 
+    private void setPrefPosPzr(){
+        prefPosPzr.add("pzr");
+    }
+
+    private void setPrefPosRecOrg(){
+        prefPosRecOrg.add("rec");
+        prefPosRecOrg.add("org");
+    }
+
     private void setWorker(){
         helga = new Worker("helga", true, true,
-                true, true, true, true, workingDaysAll, prefPosAll);
+                true, true, true, true, workingDaysAll, prefPosRecOrg);
 
         maxi = new Worker("maxi", true, true,
-                true, true, true, true, workingDaysAll, prefPosAll);
+                true, true, true, true, workingDaysAll, prefPosPzr);
 
         phoebe = new Worker("phoebe", true, true,
-                true, true, true, true, workingDaysMoWeFr, prefPosAll);
+                true, true, true, true, workingDaysAll, prefPosAll);
 
         rachel = new Worker("rachel", true, true,
-                true, true, true, true, workingDaysTuTh, prefPosAll);
+                true, true, true, true, workingDaysAll, prefPosAll);
 
         monica = new Worker("monica", true, true,
-                true, true, true, true, workingDaysTuTh, prefPosAll);
+                true, true, true, true, workingDaysAll, prefPosAll);
+
+        lilly = new Worker("lilly", true, true,
+                true, true, true, true, workingDaysAll, prefPosAll);
+
+        chandler = new Worker("chandler", true, true,
+                true, true, true, true, workingDaysAll, prefPosRecOrg);
+
+        joey = new Worker("joey", true, true,
+                true, true, true, true, workingDaysAll, prefPosPzr);
+
+        ross = new Worker("ross", true, true,
+                true, true, true, true, workingDaysAll, prefPosAll);
+
+        william = new Worker("william", true, true,
+                true, true, true, true, workingDaysAll, prefPosAll);
+
+        louis = new Worker("louis", true, true,
+                true, true, true, true, workingDaysAll, prefPosAll);
+
+        peter = new Worker("peter", true, true,
+                true, true, true, true, workingDaysAll, prefPosAll);
     }
 
 }
--- ShiftPlanner/Main.java
+++ ShiftPlanner/Main.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 public class Main {
 
     public static GenerateShiftPlanManually generateShiftPlanManually = new GenerateShiftPlanManually();
+    public static LocalDate date;
 
     public static void main(String args[]){
-    generateShiftPlanManually.initializeMorningShift();
+    MorningShift morningShift = new MorningShift(date = LocalDate.now(), new int[]{2, 1, 2, 1});
+    generateShiftPlanManually.initializeMorningShift(morningShift);
+    AfternoonShift afternoonShift = new AfternoonShift(date = LocalDate.now(), new int[]{2, 1, 2, 1});
+    generateShiftPlanManually.initializeAfternoonShift(morningShift, afternoonShift);
     }
 }
